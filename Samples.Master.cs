6c5549026f7bd00586a4d8d9aaabc5e55e400888
Jay <Jay@DOTNETDADDY>
2025-06-20 17:05:23 +0530

    Contact page

---
+++ Samples.Master.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Online.Signatures.Webforms
+{
+    public class Samples : System.Web.UI.MasterPage
+    {
+        protected System.Web.UI.HtmlControls.HtmlForm frmMainMaster;
+        protected System.Web.UI.WebControls.ContentPlaceHolder cphMain;
+        protected System.Web.UI.WebControls.ContentPlaceHolder cphFooter;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
