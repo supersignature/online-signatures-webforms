9835d2e468253132a6011429808cbd227e2f580a
Jay <Jay@DOTNETDADDY>
2025-06-21 10:07:37 +0530

    Repeater sample added

---
+++ Forms/Repeater.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="Databind Signature Instances Inside A Repeater" Language="C#" MasterPageFile="~/Samples.Master" AutoEventWireup="true" CodeBehind="Repeater.aspx.cs" Inherits="Online.Signatures.Webforms.Repeater" %>
+
+<%@ Register Assembly="SuperSignature" Namespace="SuperSignature" TagPrefix="cc1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" runat="server">
+ <asp:Repeater ID="rptSign" runat="server" onitemdatabound="rptSign_ItemDataBound">
+ <HeaderTemplate>
+   <table style="border:solid 1px #DCDCDC">
+    <tr style="background:#DCDCDC"> 
+      <td style="width:200px">Full Name</td>
+      <td>Sign</td>
+    </tr>
+ </HeaderTemplate>
+ <ItemTemplate>
+    <tr>
+     <td><h3><%#Eval("FullName")%></h3></td>
+     <td><cc1:MouseSignature ID="ctlSignature" SignWidth="250" SignHeight="150" runat="server" Enabled="true" SignRequired="true" />   </td>
+     <td><input type="button" id="btnDisable" runat="server" value="Disable" /></td>
+               <td><input type="button" id="btnEnable" runat="server" value="Enable" /></td>
+    </tr>
+    <tr><td colspan="2" style="border:solid 1px #DCDCDC"></td></tr>
+ </ItemTemplate>
+ <FooterTemplate>
+  </table>
+ </FooterTemplate>
+ </asp:Repeater>
+ <br />
+ <asp:Button ID="btnSave" runat="server" Text="Save All" onclick="btnSave_Click" />
+ </asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphFooter" runat="server">
+  <ul>
+      <li>Multiple signature instances inside a repeater</li>
+      <li>Get signature for each control server side, by looping the repeater items</li>
+    </ul>
+</asp:Content>
---
+++ Forms/Repeater.aspx.cs
@@ -0,0 +1,94 @@
+﻿using SuperSignature;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Online.Signatures.Webforms
+{
+    public partial class Repeater : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindRepeater();
+            }
+        }
+
+        private void BindRepeater()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("FullName");
+
+            DataRow dr = dt.NewRow();
+            dr["FullName"] = "John";
+            dt.Rows.Add(dr);
+
+            DataRow dr1 = dt.NewRow();
+            dr1["FullName"] = "Marie";
+            dt.Rows.Add(dr1);
+
+            DataRow dr2 = dt.NewRow();
+            dr2["FullName"] = "James";
+            dt.Rows.Add(dr2);
+
+            dt.AcceptChanges();
+            rptSign.DataSource = dt;
+            rptSign.DataBind();
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(250, 500);
+            Graphics g = Graphics.FromImage(bm);
+
+            int y = 0;
+
+            foreach (RepeaterItem rptItem in rptSign.Items)
+            {
+                MouseSignature ctlSignature;
+                ctlSignature = (rptItem.Controls[1] as MouseSignature);
+                Bitmap bmInner = ctlSignature.SaveSignature("");
+                g.DrawImage(bmInner, 0, y);
+                y = y + 160;
+            }
+
+            MemoryStream ms = new MemoryStream();
+
+            Response.ContentType = "image/png";
+
+            bm.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            ms.WriteTo(Response.OutputStream);
+
+            g.Dispose();
+            bm.Dispose();
+            Response.End();
+        }
+
+        string validateJS = " (true ";
+
+        protected void rptSign_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                string uID = (e.Item.FindControl("ctlSignature") as MouseSignature).uID;
+                (e.Item.FindControl("btnDisable") as HtmlInputButton).Attributes.Add("onclick", "SignatureEnabled('" + uID + "', false);");
+                (e.Item.FindControl("btnEnable") as HtmlInputButton).Attributes.Add("onclick", "SignatureEnabled('" + uID + "', true);");
+
+                validateJS += " && ValidateSignature('" + uID + "')";
+            }
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            btnSave.Attributes["onclick"] = "javascript:return " + validateJS + ");";
+        }
+    }
+}
