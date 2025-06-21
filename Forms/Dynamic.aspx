8e826c8c667610c2778000bce04fb1741577d69d
Jay <Jay@DOTNETDADDY>
2025-06-21 10:23:23 +0530

    Added Dynamic sample

---
+++ Forms/Dynamic.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Dynamically Render Signature Instances At Runtime" Language="C#" MasterPageFile="~/Samples.Master" AutoEventWireup="true" CodeBehind="Dynamic.aspx.cs" Inherits="Online.Signatures.Webforms.Dynamic" %>
+<asp:Content ID="ctMain" ContentPlaceHolderID="cphMain" runat="server">
+ <asp:Panel ID="pnlSign" runat="server"></asp:Panel>
+</asp:Content>
+<asp:Content ID="ctFooter" ContentPlaceHolderID="cphFooter" runat="server">
+    <ul>
+      <li>Signature instances are added runtime</li>
+    </ul>
+</asp:Content>
---
+++ Forms/Dynamic.aspx.cs
@@ -0,0 +1,26 @@
+﻿using SuperSignature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Online.Signatures.Webforms
+{
+    public partial class Dynamic : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                MouseSignature ctlSignature = new MouseSignature();
+                ctlSignature.ID = "ctlSignature" + i.ToString();
+                pnlSign.Controls.Add(ctlSignature);
+
+                // call any methods for ctlSignature after this
+
+            }
+        }
+    }
+}
