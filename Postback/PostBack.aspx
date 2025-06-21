a2de50098b5c900ea3c7af4f9a1bf720efdf6566
Jay <Jay@DOTNETDADDY>
2025-06-21 11:17:24 +0530

    Postback Sample Added

---
+++ Postback/PostBack.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Work With Signatures and Page Postback Event" Language="C#" MasterPageFile="~/Samples.Master" AutoEventWireup="true" CodeBehind="PostBack.aspx.cs" Inherits="Online.Signatures.Webforms.PostBack" %>
+<%@ Register Assembly="SuperSignature" Namespace="SuperSignature" TagPrefix="cc1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" runat="server">
+    <asp:Label ID="lblError" runat="server" ForeColor="Red" EnableViewState="false"></asp:Label><br /><br />
+    Your Name:&nbsp;<asp:TextBox ID="txtName"  runat="server"></asp:TextBox>&nbsp;(Skip this input, just sign and hit the Save button)&nbsp;<br />
+    <cc1:MouseSignature ID="ctlSignature" runat="server" SignRequired="false" SignRequiredPoints="10" />
+    <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnPostBack_Click" />&nbsp;<asp:Button
+        ID="btnClear" Text="Clear" runat="server" OnClick="btnClear_Click" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphFooter" runat="server">
+    <ul>
+        <li>Signature with postback code</li>
+    </ul>
+</asp:Content>
---
+++ Postback/PostBack.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Online.Signatures.Webforms
+{
+    public partial class PostBack : System.Web.UI.Page
+    {
+        private string SignData
+        {
+            get
+            {
+                if (null == ViewState["SignData"])
+                {
+                    return "";
+                }
+
+                return (string)ViewState["SignData"];
+            }
+            set
+            {
+                ViewState["SignData"] = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                if (ctlSignature.SignData.Length > 10)
+                {
+                    ctlSignature.SaveSignature("");
+                    SignData = ctlSignature.SignData;
+                    LoadSign();
+                }
+            }
+        }
+
+        protected void btnPostBack_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text.Trim().Length == 0)
+            {
+                lblError.Text = "Please enter name. We have loaded your signature back!";
+            }
+            else if (ctlSignature.SignData.Length > 0)
+            {
+                lblError.Text = "Thanks for signing! Now your signature got saved! Hit clear button.";
+            }
+            else
+            {
+                lblError.Text = "Sign is missing!";
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearSign();
+        }
+
+        private void ClearSign()
+        {
+            SignData = "";
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "clear", "$(window).on('load', function() {  setTimeout(function() { ClearSignature('" + ctlSignature.uID + "'); },1000); }); ", true);
+        }
+
+        private void LoadSign()
+        {
+            if (ctlSignature.SignData.Length > 0)
+            {
+                ctlSignature.LoadSignature(SignData, "1");
+            }
+        }
+    }
+}
